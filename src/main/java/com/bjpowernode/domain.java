9592f1570ee2a68a957c4969383cd2fae3a20437
wangjinlei5587 <81354274+wangjinlei5587@users.noreply.github.com>
2021-06-05 20:09:05 +0800

    11

--- src/main/java/com/bjpowernode/domain.java
+++ src/main/java/com/bjpowernode/domain.java
@@ -2,7 +2,7 @@ package com.bjpowernode;
 
 public class domain {
     public static void main(String[] args) {
-        System.out.println("这是dev吗");
+        System.out.println("这是dev啊");
         
     }
 }
