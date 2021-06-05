a56b32195f94665e15ec8b56943fe18a999bb9a0
wangjinlei5587 <81354274+wangjinlei5587@users.noreply.github.com>
2021-06-05 20:16:45 +0800

    Update domain.java

--- src/main/java/com/bjpowernode/domain.java
+++ src/main/java/com/bjpowernode/domain.java
@@ -2,7 +2,7 @@ package com.bjpowernode;
 
 public class domain {
     public static void main(String[] args) {
-        System.out.println("这是dev啊");
+        System.out.println("hello dev");
         
     }
 }
