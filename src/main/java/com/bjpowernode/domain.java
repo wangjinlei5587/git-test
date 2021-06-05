a379ff7bff34b81363fe5600223ebb3c39cea5c5
wangjl13 <157771362@qq.com>
2021-06-05 19:38:09 +0800

    push到dev commit

--- src/main/java/com/bjpowernode/domain.java
+++ src/main/java/com/bjpowernode/domain.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class domain {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("push 到dev");
     }
 }
