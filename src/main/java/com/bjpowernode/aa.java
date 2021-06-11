29db8027cc5ff14c3de27c5a72ae570939fb21b6
wangjl13 <157771362@qq.com>
2021-06-11 21:48:53 +0800

    aa的第二次修改

--- src/main/java/com/bjpowernode/aa.java
+++ src/main/java/com/bjpowernode/aa.java
@@ -1,4 +1,11 @@
 package com.bjpowernode;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class aa {
+
+    public static void main(String[] args) {
+        System.out.println("啊哈哈哈哈");
+        System.out.println("第二次修改");
+    }
 }
