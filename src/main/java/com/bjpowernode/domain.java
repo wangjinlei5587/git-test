8a680a93e18f295346c87e49e106778c2ad47b95
wangjl13 <157771362@qq.com>
2021-06-11 21:51:35 +0800

    rollback

--- src/main/java/com/bjpowernode/domain.java
+++ src/main/java/com/bjpowernode/domain.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class domain {
     public static void main(String[] args) {
         System.out.println("hello dev");
+        System.out.println("rollback");
     }
 }
